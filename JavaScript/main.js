b4f67302b3ea550fd66a6672a43e70b78145d324
qMedop <95769954+qMedop@users.noreply.github.com>
2023-05-27 15:22:08 +0300

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -484,3 +484,23 @@ video.onwaiting = (event) => {
 video.onplaying = (event) => {
   videoContainer.classList.remove('loading')
 };
+document.addEventListener('contextmenu' ,e => {
+  e.preventDefault()
+})
+document.onkeydown = (e) => {
+  if (e.key == 123) {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == 'U') {
+      e.preventDefault();
+  }
+};
